bd1016c80f8d8f3a0d031ba863ca6b8ed3c37874
Dianis Caro <dm.caro@uniandes.edu.co>
2019-10-19 17:01:35 -0500

    VOs - Interface

---
+++ src/main/java/negocio/VOAfiliado.java
@@ -0,0 +1,31 @@
+package negocio;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface VOAfiliado 
+{
+	public Date getFechaDeNacimiento();
+
+	public void setFechaDeNacimiento(Date fechaDeNacimiento);
+
+	public String getNombre();
+
+	public void setNombre(String nombre);
+
+	public String getNumeroDeDocumento();
+
+	public void setNumeroDeDocumento(String numeroDeDocumento);
+
+	public TipoDeDocumento getTipoDocumento();
+
+	public void setTipoDocumento(TipoDeDocumento tipoDocumento);
+
+	public List<Long> getCitasRecervadas();
+
+	public void setCitasRecervadas(List<Long> citasRecervadas);
+
+	public List<Long> getOrdenes();
+	
+	public void setOrdenes(List<Long> ordenes) ;
+}
---
+++ src/main/java/negocio/VOTerapia.java
@@ -0,0 +1,8 @@
+package negocio;
+
+public interface VOTerapia extends VOServicioDeSalud {
+
+	public String getTipoTerapia();
+
+	public void setTipoTerapia(String tipoTerapia);
+}
